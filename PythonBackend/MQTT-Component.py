aa2515f9bddda9704c5cbdb5174c043b563a7b9f
MunsMan <hendrik.munske@me.com>
2020-01-25 14:42:56 +0100

    First steps for the MQTT-Client

---
+++ PythonBackend/MQTT-Component.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+from Objects import BaseObject
+
+
+class MQTTClient(BaseObject):
+    def __init__(self):
+        self.client = mqtt.Client()
+
+    def connect(self):
+        q = self.client.connect("localhost", 1883)
+        return "Connection to localhost, Quality {i}".format(i=q)
+
+
+if __name__ == '__main__':
+
+    c = MQTTClient()
+    c.name = "Client"
+    print(c.get_attribute())
