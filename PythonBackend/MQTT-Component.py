ff470c21afb4ffb34ffe7098feba313a534f344e
MunsMan <hendrik.munske@me.com>
2020-01-26 10:42:41 +0100

    creating the on_connect with gets connected with the log function

--- PythonBackend/MQTT-Component.py
+++ PythonBackend/MQTT-Component.py
@@ -10,6 +10,15 @@ class MQTTClient(BaseObject):
         q = self.client.connect("localhost", 1883)
         return "Connection to localhost, Quality {i}".format(i=q)
 
+    def init(self):
+        pass
+
+    def on_connect(self):
+        """
+        Logging for debugging
+        :return:
+        """
+        pass
 
 if __name__ == '__main__':
 
