7ac32d7ab4a729b2e7e9741d7b247afe69d83f66
MunsMan <hendrik.munske@me.com>
2020-01-26 19:28:30 +0100

    Testing and Debugging

--- PythonBackend/Log.py
+++ PythonBackend/Log.py
@@ -1,24 +1,30 @@
 import os
 import time
 
+
 class Log:
-    def __init__(self, id):
-        self.id = id
-        self.log_name = self.get_name
+    def __init__(self, name):
+        self.id = name
+        self.log_name = self.get_name()
+        self.create_log()
 
     def get_name(self):
-        return "log"+"<"+str(self.id)+">.txt"
+        return "Log-<"+str(self.id)+">.txt"
 
     def create_log(self):
         path = os.path.dirname(__file__)
         path = os.path.join(path, "log")
         f = open(str(self.log_name), 'w+')
         f.write("Log of {n}: \n".format(n=self.id))
+        f.close()
 
-    @staticmethod
-    def log_post(post):
-        log_head = "log " + str(time.asctime()) + ": " + str(post)
-        return log_head
+    def log_post(self, post):
+        log_head = str(time.asctime()) + ": " + str(post)
+        f = open(self.log_name, "a")
+        f.write(log_head)
+        f.close()
 
 
-
+if __name__ == '__main__':
+    TestLog = Log("Test")
+    TestLog.log_post("Connection found")
