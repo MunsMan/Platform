30f87ffc21687470400178fb18c7adac8f218743
MunsMan <hendrik.munske@me.com>
2020-01-27 20:12:09 +0100

    First running Beta, still a lot to do

--- PythonBackend/Extensions/MQTT-Component.py
+++ PythonBackend/Extensions/MQTT-Component.py
@@ -1,28 +1,48 @@
+import time
+import json
 import paho.mqtt.client as mqtt
-from Objects import BaseObject
 
 
-class MQTTClient(BaseObject):
+class MQTTClient():
     def __init__(self):
         self.client = mqtt.Client()
+        self.client.on_message = self.on_message
+        self.client.on_connect = self.on_connect
 
     def connect(self):
-        q = self.client.connect("localhost", 1883)
+        q = self.client.connect("169.254.248.10", 1883)
         return "Connection to localhost, Quality {i}".format(i=q)
 
     def init(self):
         pass
 
-    def on_connect(self):
+    def subscriber(self, topic):
+        q = self.client.subscribe(topic)
+        return "Subscribed to Topic {t} with the Quality {q}".format(t=topic, q=q)
+
+    def on_message(self, client, userdata, msg):
+        print(json.loads(msg.payload))
+
+    def on_connect(self, client, userdata):
         """
         Logging for debugging
         :return:
         """
         pass
 
+    def main(self):
+        try:
+            print(self.connect())
+            time.sleep(4)
+            print(self.subscriber("/RASPI/INTERN/CPU/TEMP"))
+            self.client.loop_forever()
+
+        finally:
+            self.client.disconnect()
+
+
 
 if __name__ == '__main__':
 
     c = MQTTClient()
-    c.name = "Client"
-    print(c.get_attribute())
+    c.main()
