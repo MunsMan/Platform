8b67305596d1be70f8e658410daf160160d7bb1f
MunsMan <hendrik.munske@me.com>
2020-01-28 21:41:44 +0100

    Adding a scanning function first tests positive

--- PythonBackend/Extensions/MQTT-Component.py
+++ PythonBackend/Extensions/MQTT-Component.py
@@ -3,18 +3,21 @@ import json
 import paho.mqtt.client as mqtt
 
 
-class MQTTClient():
-    def __init__(self):
+class MQTTClient:
+    def __init__(self, device_name):
         self.client = mqtt.Client()
+        self.device_name = device_name
         self.client.on_message = self.on_message
         self.client.on_connect = self.on_connect
+        self.full_tree_scan_store = []
+        self.topic_list = []
 
     def connect(self):
-        q = self.client.connect("169.254.248.10", 1883)
+        q = self.client.connect("localhost", 1883)
         return "Connection to localhost, Quality {i}".format(i=q)
 
     def init(self):
-        pass
+        self.client.loop_start()
 
     def subscriber(self, topic):
         q = self.client.subscribe(topic)
@@ -30,19 +33,41 @@ class MQTTClient():
         """
         pass
 
+    def full_tree_scan(self):
+        base_topic = "/{d}/#"
+        self.client.subscribe(base_topic)
+        pre_on_message = self.client.on_message
+        self.client.on_message = self.on_message_scan
+        print("listening for 10 sec for messages")
+        time.sleep(10)
+        data = self.full_tree_scan_store
+        self.full_tree_scan_store = []
+        self.client.on_message = pre_on_message
+        return data
+
+    def on_message_scan(self, client, userdata, msg):
+        if msg.topic not in self.topic_list:
+            self.topic_list.append(msg.topic)
+            self.full_tree_scan_store.append({"topic": msg.topic, "timestamp": msg.timestamp, "data": msg.payload})
+
     def main(self):
         try:
+            print("Connecting")
             print(self.connect())
+            time.sleep(1)
+            self.init()
             time.sleep(4)
             print(self.subscriber("/RASPI/INTERN/CPU/TEMP"))
-            self.client.loop_forever()
+            print(self.full_tree_scan())
+            time.sleep(1)
+            while True:
+                time.sleep(1)
 
         finally:
+            self.client.loop_stop()
             self.client.disconnect()
 
 
-
 if __name__ == '__main__':
-
-    c = MQTTClient()
+    c = MQTTClient("RASPI")
     c.main()
