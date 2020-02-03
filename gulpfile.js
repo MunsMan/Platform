5cbb441edc86cd0105e7c8ab5339abb0c30002d4
BuildTools <unconfigured@null.spigotmc.org>
2020-02-03 22:25:04 +0100

    gulp supp. added, include-media, sass, browsersync

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+// Compiler
+function style() {
+  return gulp
+    .src("./web/src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./web/src/css"))
+    .pipe(browserSync.stream());
+}
+// Watch
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./web"
+    }
+  });
+  gulp.watch("./web/src/scss/**/*.scss", style);
+  gulp.watch("./web/src/scss/**/*.sass", style);
+  gulp.watch("./web/src/**/*.html").on("change", browserSync.reload);
+  gulp.watch("./web/src/**/*.php").on("change", browserSync.reload);
+  gulp.watch("./web/src/**/*.js").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
